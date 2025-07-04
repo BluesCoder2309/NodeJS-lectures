0006ea46b03df54d935f20ea94baccf55a3bafff
BluesCoder2309 <singh.utkarsh2309@gmail.com>
2025-07-04 23:55:28 +0530

    NodeJS Lecture

---
+++ 01/blocking.js
@@ -0,0 +1,6 @@
+for(let i = 1; i<10000000000; i++){
+    //Runs just consuming time
+}
+console.log('Task to be performed after the loop');
+console.log('Prints next as waiting for loop to finish');
+
---
+++ 01/non-blocking.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+  console.log("Task to be performed after 8 seconds.");
+}, 8000);
+
+console.log("Don't have to wait for above task to finish");
