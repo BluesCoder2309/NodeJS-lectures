4f1340eaa9ceffff21ff703454df6a12f1258c85
BluesCoder2309 <singh.utkarsh2309@gmail.com>
2025-07-07 18:24:28 +0530

    Modules in NodeJS using ES6

---
+++ Modules in NodeJS/02/airthmetic.mjs
@@ -0,0 +1,15 @@
+// ii) ES6 Module
+//Export module using ES6 syntax... Just add export before the function or property name
+
+export function sum(x, y) {
+  return x + y;
+}
+
+export const num = 100;
+
+function div(x, y) {
+  return x / y;
+}
+
+
+
