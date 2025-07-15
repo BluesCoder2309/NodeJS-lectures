ed1494396c1fcf46d4b48b26ac2d7bf01c48d48b
BluesCoder2309 <singh.utkarsh2309@gmail.com>
2025-07-15 18:02:52 +0530

    Learn about node inspect command for debugging

---
+++ More Modules/product.js
@@ -0,0 +1,20 @@
+// Debugging in NodeJS
+
+function calculateTotal(products) {
+  let total = 0;
+
+  products.forEach((product) => {
+    total += product.price * product.quantity;
+  });
+  return total;
+}
+
+const productList = [
+  { name: "Shoes", price: 50, quantity: 2 },
+  { name: "Hat", price: 25, quantity: 1 },
+  { name: "Gloves", price: 30, quantity: 2 },
+];
+
+//expected result => 100=25=60 = 185
+const grandTotal = calculateTotal(productList);
+console.log('Grand Total: ',grandTotal);
